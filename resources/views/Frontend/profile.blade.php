6b957b1c85d90a910527952ca160a0fa3d3a938a
Jayve <anhtan3396@gmail.com>
2021-11-07 20:10:45 +0700

    Auto stash before rebase of "develop"

---
+++ resources/views/Frontend/profile.blade.php
@@ -0,0 +1,17 @@
+
+@extends('Frontend.masterpage.masterpage')
+@section('titleForm')
+        <h4>Trang chủ</h4>
+@endsection
+@section('content')
+
+ <div class="main-content">
+    <div class="container">
+
+      </div>
+  </div>
+
+<script>
+  /
+</script>
+@endsection
